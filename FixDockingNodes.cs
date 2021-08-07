466079ad1d956aa78f51f1d28287c63baa5c473e
JPLRepo <jamieleighton69@gmail.com>
2021-08-07 11:10:20 +1000

    Initial

---
+++ FixDockingNodes.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace FixDockingNodes
+{
+    public class ModuleDockingNodeFixed : ModuleDockingNode
+    {
+        protected override void ApplyCoordsUpdate()
+        {
+            if (HighLogic.LoadedSceneIsFlight && canRotate && !nodeIsLocked && otherNode != null && vessel != null && part.vessel != null)
+            {
+                RecurseCoordUpdate(part.vessel.rootPart, part.vessel.rootPart);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FixDockingNodes")]
+[assembly: AssemblyDescription("Addon MOD for KerbalSpaceProgram")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jamie Leighton")]
+[assembly: AssemblyProduct("FixDockingNodes")]
+[assembly: AssemblyCopyright("Copyright © Jamie Leighton 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9cb78d19-8451-4a6e-8874-9881737b234d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: KSPAssembly("FixDockingNodes", 1, 0)] 
